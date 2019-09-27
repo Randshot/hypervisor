d8113d5d5b3310be1d0d2dfca2d4e8bd3eabc264
Randshot <randshot@norealm.xyz>
2019-09-27 18:06:29 +0200

    feat: add path for WDK 10.0.18362.0 feat: add path for VS2019 and remove VS2015
    Signed-off-by: Randshot <randshot@norealm.xyz>

--- scripts/util/driver_build.sh
+++ scripts/util/driver_build.sh
@@ -23,18 +23,18 @@
 # $1 == CMAKE_SOURCE_DIR
 # $2 == CMAKE_INSTALL_PREFIX
 
-msbuild_2015="/cygdrive/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe"
-msbuild_2017="/cygdrive/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/bin/msbuild.exe"
+msbuild_2017="/cygdrive/a/VS/2017/MSBuild/15.0/bin/msbuild.exe"
+msbuild_2019="/cygdrive/a/VS/2019/MSBuild/Current/bin/msbuild.exe"
 
 find_msbuild() {
 
-    if [[ -f $msbuild_2017 ]]; then
-        msbuild=$msbuild_2017
+    if [[ -f $msbuild_2019 ]]; then
+        msbuild=$msbuild_2019
         return
     fi
 
-    if [[ -f $msbuild_2015 ]]; then
-        msbuild=$msbuild_2015
+    if [[ -f $msbuild_2017 ]]; then
+        msbuild=$msbuild_2017
         return
     fi
 
